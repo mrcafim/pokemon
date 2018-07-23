bec13d8790697c4972b16c7ce18222e77dc74635
Matheus Alves <carvalho.matheus1997@gmail.com>
2018-07-23 05:18:25 -0300

    Fixed some problems about AJAX Request, almost ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,142 @@
-$(document).ready(function(){   
+$(document).ready(function(){
+	$('#loading').hide();
+	$('#infoPikachu').hide();
+	$('#infoDitto').hide();
+	$('#infoCharizard').hide();
+	$('#infoLugia').hide();
+	$('#infoSquirtle').hide();
+
+
 	$('#btnPikachu').click(function(){
 		event.preventDefault();
-
+		$('#loading').show();
+		var retorno = '';
 		$.ajax({
 			async: true,
 			crossDomain: true,
 			type: 'GET',
-			url: 'https://pokeapi.co/api/v2/pokemon/2',
+			url: 'https://pokeapi.co/api/v2/pokemon/25',
       		dataType: 'json',
 			headers: {
 			},
 			success: function(data) {
-				console.log(data);
+				$('#infoPikachu').show();
+				$('#loading').hide();
+				retorno += '<strong> Nome:</strong>';
+				retorno += ' '+data.name+'<br>';
+				retorno += '<strong> Id:</strong>';
+				retorno += ' '+data.id+'<br>';
+				retorno += '<strong> Tipo:</strong>';
+				retorno += ' '+data.type+'<br>';
+				$('#infoPikachu').html(retorno);
+			}
+		});
+
+	});
+
+	$('#btnDitto').click(function(){
+		event.preventDefault();
+		$('#loading').show();
+		var retorno = '';
+		$.ajax({
+			async: true,
+			crossDomain: true,
+			type: 'GET',
+			url: 'https://pokeapi.co/api/v2/pokemon/132',
+      		dataType: 'json',
+			headers: {
+			},
+			success: function(data) {
+				$('#infoDitto').show();
+				$('#loading').hide();
+				retorno += '<strong> Nome:</strong>';
+				retorno += ' '+data.name+'<br>';
+				retorno += '<strong> Id:</strong>';
+				retorno += ' '+data.id+'<br>';
+				retorno += '<strong> Tipo:</strong>';
+				retorno += ' '+data.type+'<br>';
+				$('#infoDitto').html(retorno);
+			}
+		});
+
+	});
+
+	$('#btnCharizard').click(function(){
+		event.preventDefault();
+		$('#loading').show();
+		var retorno = '';
+		$.ajax({
+			async: true,
+			crossDomain: true,
+			type: 'GET',
+			url: 'https://pokeapi.co/api/v2/pokemon/6',
+      		dataType: 'json',
+			headers: {
+			},
+			success: function(data) {
+				$('#infoCharizard').show();
+				$('#loading').hide();
+				retorno += '<strong> Nome:</strong>';
+				retorno += ' '+data.name+'<br>';
+				retorno += '<strong> Id:</strong>';
+				retorno += ' '+data.id+'<br>';
+				retorno += '<strong> Tipo:</strong>';
+				retorno += ' '+data.type+'<br>';
+				$('#infoCharizard').html(retorno);
+			}
+		});
+
+	});
+
+	$('#btnLugia').click(function(){
+		event.preventDefault();
+		$('#loading').show();
+		var retorno = '';
+		$.ajax({
+			async: true,
+			crossDomain: true,
+			type: 'GET',
+			url: 'https://pokeapi.co/api/v2/pokemon/249',
+      		dataType: 'json',
+			headers: {
+			},
+			success: function(data) {
+				$('#infoLugia').show();
+				$('#loading').hide();
+				retorno += '<strong> Nome:</strong>';
+				retorno += ' '+data.name+'<br>';
+				retorno += '<strong> Id:</strong>';
+				retorno += ' '+data.id+'<br>';
+				retorno += '<strong> Tipo:</strong>';
+				retorno += ' '+data.type+'<br>';
+				$('#infoLugia').html(retorno);
+			}
+		});
+
+	});
+
+	$('#btnSquirtle').click(function(){
+		event.preventDefault();
+		$('#loading').show();
+		var retorno = '';
+		$.ajax({
+			async: true,
+			crossDomain: true,
+			type: 'GET',
+			url: 'https://pokeapi.co/api/v2/pokemon/7',
+      		dataType: 'json',
+			headers: {
+			},
+			success: function(data) {
+				$('#infoSquirtle').show();
+				$('#loading').hide();
+				retorno += '<strong> Nome:</strong>';
+				retorno += ' '+data.name+'<br>';
+				retorno += '<strong> Id:</strong>';
+				retorno += ' '+data.id+'<br>';
+				retorno += '<strong> Tipo:</strong>';
+				retorno += ' '+data.type+'<br>';
+				$('#infoSquirtle').html(retorno);
 			}
 		});
 
